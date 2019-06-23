792e5ec31929fb823cdc85ffa408b5bf7233e8e7
Vipul Sharma <vipulsharma018@gmail.com>
2019-06-22 22:07:55 -0700

    Project init

---
+++ ppmtool/src/main/java/me/theorganizedchaos/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package me.theorganizedchaos.ppmtool.repositories;
+
+import me.theorganizedchaos.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+
+}
